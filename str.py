3c482dc9b204699e4594588375f8e2570c657666
nat-king-15 <66466499+nat-king-15@users.noreply.github.com>
2021-10-19 19:45:28 +0530

    Update str.py

--- str.py
+++ str.py
@@ -5,7 +5,7 @@ import asyncio
 from pyrogram import Client
 
 
-print("Enter your app information from my.eSport.com/apps below.")
+print("Enter your app information from my.Natking.com/apps below.")
 
 
 async def main():
